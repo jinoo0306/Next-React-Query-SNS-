4775d5f82068fae58038b4d6054ced886599c408
j._.jinoo <jinoo0306@naver.com>
2024-08-26 23:43:09 +0900

    session : ch2 이벤트 캡처링과 /status/[id] 페이지

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import BackButton from "@src/app/(afterLogin)/_components/BackButton";
+import style from "./singlePost.module.css";
+import Post from "@src/app/(afterLogin)/_components/Post";
+import CommentForm from "@src/app/(afterLogin)/[username]/status/[id]/_components/CommentForm";
 
-function page() {
-  return <div></div>;
+export default function SinglePost() {
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
-
-export default page;
--- src/app/(afterLogin)/_components/Post.tsx
+++ src/app/(afterLogin)/_components/Post.tsx
@@ -4,13 +4,21 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
 import ActionButtons from "@src/app/(afterLogin)/_components/ActionButtons";
+import PostArticle from "@src/app/(afterLogin)/_components/PostArticle";
+import { faker } from "@faker-js/faker";
+import PostImages from "@src/app/(afterLogin)/_components/PostImages";
+
 import Image from "next/image";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
 
-export default function Post() {
+type Props = {
+  noImage?: boolean;
+};
+export default function Post({ noImage }: Props) {
   const target = {
+    postId: 1,
     User: {
       id: "elonmusk",
       nickname: "Elon Musk",
@@ -18,10 +26,19 @@ export default function Post() {
     },
     content: "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
   };
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() },
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 4, link: faker.image.urlLoremFlickr() }
+    );
+  }
+
   return (
-    <article className={style.post}>
+    <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link href={`/${target.User.id}`} className={style.postUserImage}>
@@ -47,10 +64,12 @@ export default function Post() {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}></div>
+          <div>
+            <PostImages post={target} />
+          </div>
           <ActionButtons />
         </div>
       </div>
-    </article>
+    </PostArticle>
   );
 }
